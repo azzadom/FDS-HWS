918c582691a4a5417b6fcb8c36a232384b98fa30
Domenico Azzarito <domazza6902@outlook.it>
2024-11-21 23:31:17 +0100

    Update 2
    Add report at point 2.2

--- Homework02/libs/models/poor_cnn.py
+++ Homework02/libs/models/poor_cnn.py
@@ -3,7 +3,7 @@ import torch
 
 class PoorPerformingCNN(nn.Module):
     def __init__(self):
-        super(PoorPerformingCNN, self).__init__()
+        super().__init__()
         ##############################
         ###     CHANGE THIS CODE   ###
         ##############################  
@@ -11,7 +11,9 @@ class PoorPerformingCNN(nn.Module):
         self.relu1 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.conv2 = nn.Conv2d(5, 8, kernel_size=3, stride=1, padding=1)
+
+        # Changed input channels from 5 to 4 to match conv1 output
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
 
         self.fc1 = nn.Linear(8 * 4 * 4, 28)
@@ -19,6 +21,7 @@ class PoorPerformingCNN(nn.Module):
     def forward(self, x):
         x = self.pool(self.relu1(self.conv1(x)))
         x = self.pool(self.relu2(self.conv2(x)))
-        x = x.view(-1, 8 * 8 * 8)
+        # Fixed the flatten operation to match actual dimensions
+        x = x.view(-1, 8 * 4 * 4)
         x = self.fc1(x)
         return x
